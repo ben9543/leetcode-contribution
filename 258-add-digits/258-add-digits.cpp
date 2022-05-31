e079977ff7635d6d46a9fedb632f579eea6cc0ed
Sunbeom Kweon (Ben) <bennie.ben095@gmail.com>
2022-05-30 17:05:01 -0700

    Time: 4 ms (38.35%), Space: 5.8 MB (95.46%) - LeetHub

---
+++ 258-add-digits/258-add-digits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int result = 0;
+        while (num > 0){
+            result += num%10;
+            num = floor(num/10);
+        }
+        if (result >= 10)return addDigits(result);
+        else return result;
+    }
+};
