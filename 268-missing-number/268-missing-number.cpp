c544cf6eb1adcbf36b8548ab1ef703ebccd15cb9
Sunbeom Kweon (Ben) <bennie.ben095@gmail.com>
2022-06-06 16:49:33 -0700

    Time: 24 ms (68.51%), Space: 18 MB (19.19%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         vector<bool> r(nums.size()+1);
-        for (auto num: nums){
+        for (auto num: nums)
             r[num] = true;
-        }
-        for (int i = 0; i < r.size(); i++){
+        
+        for (int i = 0; i < r.size(); i++)
             if (!r[i]) return i;
-        }
+        
         return nums.size();
     }
 };
