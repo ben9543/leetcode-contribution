78dfe3ee1461e0361e9156bf423fde5bb9c88e35
Sunbeom Kweon (Ben) <bennie.ben095@gmail.com>
2022-06-16 14:29:42 -0700

    Time: 0 ms (100.00%), Space: 6.1 MB (32.65%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -2,13 +2,29 @@
 class Solution {
 public:
     int titleToNumber(string columnTitle) {
-        int size = columnTitle.length();
+        // 1. Understanding
+        // Input: string 
+        // Output: int
+        // 2. Matching
+        // Extracting a digit | bit from an integer
+        // Loop, either for or while => For loop with backward looping 
+        // Use ASCII table
+        // 3. Planning
+        // Backward loop is gonna start from the length of the string - 1 => we need to access the least significant digit => last element of the string.
+        // 4. Implementing
+        int l = columnTitle.length()-1;
+        int result = 0;
         int count = 0;
-        int exp = 0;
-        for (int i = size-1; i >=0 ; i--){
-            count += (columnTitle[i] - 64)*pow(26, exp);
-            exp++;
+        for (int i = l; i >= 0; i--){
+            // 'A' => 65
+            // 'A' - 64 = 1
+            // 'ABC'
+            int n = columnTitle[i] - 64; // C(67) - 64 => 3
+            
+            // Each digit represents 26
+            result += (n * pow(26, count));
+            count++;
         }
-        return count;
+        return result;
     }
 };
