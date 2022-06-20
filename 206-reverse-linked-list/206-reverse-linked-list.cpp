1a19e8eee36d952942b887d64c7dbbb0ce60c24d
Sunbeom Kweon (Ben) <bennie.ben095@gmail.com>
2022-06-20 08:21:31 -0700

    Time: 7 ms (76.82%), Space: 8.3 MB (41.97%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -11,10 +11,32 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+        
+        // currNode points wherever the current node we are working on.
+        // prevNode will be the linkedlist we are going to build up.
+        // nextNode is simply a storage to hold the next pointer of currNode,
+        //          so that we do not lose the connection to the given  linkedlist.
         ListNode* nextNode, *prevNode = nullptr, *currNode = head;
+        
         while (currNode){
+            
+            // 1. Move nextNode to currNode's next before we lose the connection.
             nextNode = currNode->next;
+            
+            // 2. Now detach currNode from nextNode, attach it to prevNode
             currNode->next = prevNode;
+            
+            // 3. Since currNode is now attached to prevNode, 
+            // we can simply move the prevNode to its starting point, which is currNode.
+            // And currNode need to be updated as nextNode for the next loop (nextNode = currNode->next)
+            // The location of nextNode is not important at the point.
+            // 
+            // 
+            //       prev   curr    next   
+            //  () <- () <- ()      ()
+            //
+            //             prev  curr&next   
+            //  () <- () <- ()      ()
             prevNode = currNode;
             currNode = nextNode;
         }
