06cfcec60e3cc2e7bb3ddf8730d9fd92b33f6653
Sunbeom Kweon (Ben) <bennie.ben095@gmail.com>
2022-06-27 14:56:53 -0700

    Update PriorityQueue.cpp

--- PriorityQueue.cpp
+++ PriorityQueue.cpp
@@ -1,8 +1,16 @@
 #include <vector>;
+#include <algorithm>; // std::swap
+using namespace std;
 
 class PriorityQueue {
 private:
   vector<int> heap;
+  int parent(int index){ return floor((index-1)/2); }
+  int left(int index){ return (index*2 + 1); }
+  int right(int index){ return (index*2 + 2); }
+  void swap(int i, int j){
+    swap(this->heap[i], this->heap[j]);
+  }
   
 public:
   PriorityQueue(){
