1185386a6822f829e7951b93770dc801a5e9e698
Sunbeom Kweon (Ben) <bennie.ben095@gmail.com>
2022-07-06 22:04:56 -0700

    Time: 353 ms (40.99%), Space: 10.4 MB (33.57%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
     int countPrimes(int n) {
+        // 1. Understanding
+        // Input: integer => range of the numbers
+        // Output: integer => number of the primes
+        // 2. Matching
+        // isPrime => recursive & heavy ...
+        // 3. Planning
+        // hashmap => that stores the information of if the integer is a prime or not.
+        //         => so that we don't have to calculate whether the integer is a prime or not all the way from the bottom
+        //         the way how to find if n is a prime: i%m => prime  see if the modulo function gives you a 0
+        //   1) Need to store primes to the hashmap
+        //   2) If the hashmap says the number is not visited, the number is prime. 
+        //   3) In the every loop I am going to iterate all multiples of the prime that we found.
+        //      2 => 2*2 , 2*3 .. < n => false
+        // 4. Implementation
         
         // Base case
-        if (n == 1 or n == 0 or n == 2) return 0;
+        if(n == 0 || n == 1 || n == 2)return 0;
         
-        // Implementation
+        vector<bool> isPrimes(n+2, true);
         int count = 0;
-        vector<bool> isPrime(n+2, true);
-        
         for (int i = 2; i < n; i++){
-            if(isPrime[i]){
-                count++;
+            
+            // Check if the number is a prime
+            if(isPrimes[i]){
                 
-                // Mark all multiples of i as false
-                for(int j = 2; i*j < n; j++){
-                    isPrime[i*j] = false;
+                count++;
+                // Checking all the multiples of the prime
+                for (int j = 2; i*j < n; j++){
+                    isPrimes[i*j] = false;
                 }
             }
         }
-        
         return count;
     }
 };
