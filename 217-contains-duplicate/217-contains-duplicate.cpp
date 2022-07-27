782c47b1fba03f5203f09800e4b15e8358f2ff19
Sunbeom Kweon (Ben) <bennie.ben095@gmail.com>
2022-07-26 22:49:41 -0700

    Time: 107 ms (86.73%), Space: 51.5 MB (50.99%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,15 +1,15 @@
-#include<map>
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int, bool> seen;
-        for(int i = 0; i < nums.size(); i++){
-            if(seen.count(nums[i])){
+        unordered_set<int> s;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (s.find(nums[i]) != s.end()) {
                 return true;
-            } else {
-                seen[nums[i]] = true;
             }
+            s.insert(nums[i]);
         }
+        
         return false;
     }
 };
