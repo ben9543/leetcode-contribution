0d57f467eb782e4f5ebff9b39800cbf87142e677
Sunbeom Kweon (Ben) <bennie.ben095@gmail.com>
2022-07-28 16:14:13 -0700

    Time: 158 ms (46.55%), Space: 21 MB (49.52%) - LeetHub

---
+++ 15-3sum/15-3sum.cpp
@@ -0,0 +1,36 @@
+#include<map>
+#include<algorithm>
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>res;
+        
+        // Sort the array first. This is for removing duplicates & two pointer search.
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < nums.size(); i++)
+        {
+            
+            if(i > 0 && nums[i] == nums[i-1])continue;
+            
+            int low = i+1;
+            int high = nums.size() - 1;
+            
+            while(low < high)
+            {
+                int sum = nums[low] + nums[high] + nums[i];
+                if(sum < 0) low++;
+                else if(sum > 0) high--;
+                else{
+                    vector<int> tmp = {nums[low], nums[high], nums[i]};
+                    res.push_back(tmp);
+                    low++;
+                    while(nums[low] == nums[low-1] && low < high)low++;
+                }
+            }
+        }
+        
+        return res;
+    }
+};
