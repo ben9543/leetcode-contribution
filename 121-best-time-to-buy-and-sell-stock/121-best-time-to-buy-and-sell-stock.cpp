c9f6b2d76daae99eeecfe458abf033523e1ab1f6
Sunbeom Kweon (Ben) <bennie.ben095@gmail.com>
2022-08-31 00:18:15 -0700

    Time: 339 ms (5.05%), Space: 93.3 MB (52.04%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int buy = 0; // Should be less
-        int sell = 0; // Should be big
-        int maxP = 0;
-        for(int i = 0; i < prices.size(); i++)
-        {
-            // Greedy
-            if(prices[buy] > prices[i]) buy = i;
-            if(prices[sell] < prices[i]) sell = i;
-            // Update sell if it is less than buy (cannot sell before buy it.)
-            if(sell < buy)sell = buy;
-            maxP = max(maxP, prices[sell] - prices[buy]);
+        int buy = INT_MAX;
+        int profit = 0;
+        for(int i = 0; i < prices.size(); i++){
+            buy = min(prices[i], buy);
+            profit = max(profit, prices[i] - buy);
         }
-        return maxP;
+        return profit;
     }
 };
