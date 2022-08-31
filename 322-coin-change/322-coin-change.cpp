feaf2f0194292a5b5a2ce4b24281188369a12d94
Sunbeom Kweon (Ben) <bennie.ben095@gmail.com>
2022-08-30 21:54:35 -0700

    Time: 89 ms (87.20%), Space: 14.6 MB (55.41%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,35 +2,23 @@ class Solution {
 public:
     
     vector<int> dp;
+    int MAX_AMOUNT;
     
     int coinChange(vector<int>& coins, int amount) {
-        // 1. Understanding
-        //  Coins can be reused
-        //  There could be no answer 
-        
-        // 2. Matching
-        //  Dynamic Programming => Because "fewest"
-        
-        // 3. Planning
-        //  By subtracting each coin until the amount gets to zero. 
-        //  We need to update the return value by finding the fewest num of coins we used.
-        // dp[i] represents amount of coins used to get i
-        
-        
-        // Construct DP
-        dp = vector<int> (amount+1, amount+1);
+        MAX_AMOUNT = amount + 1;
+        dp = vector(MAX_AMOUNT, MAX_AMOUNT);
         dp[0] = 0;
-        for(int a = 1; a <= amount; a++){
-            for(int j = 0; j < coins.size(); j++){
-                
-                // Can be a valid answer
-                if(a - coins[j] >= 0){
-                    dp[a] = min(dp[a], 1 + dp[a - coins[j]]);
-                }
+        
+        for(int a = 1; a <= amount; a++)
+        {
+            for(auto c : coins)
+            {
+                if(a-c >= 0)
+                    dp[a] = min(dp[a], dp[a-c] + 1);
             }
         }
         
-        return dp[amount] != amount+1 ? dp[amount] : -1;
+        return (dp[amount] == MAX_AMOUNT) ? -1 : dp[amount];
+         
     }
-
 };
