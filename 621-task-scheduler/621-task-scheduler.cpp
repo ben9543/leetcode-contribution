fb0b1c8fdb21fd03e6dcb829627c7a0d4a802643
Sunbeom Kweon (Ben) <sunbeom.kweon@gmail.com>
2022-10-01 11:06:56 -0700

    Time: 228 ms (19.27%), Space: 38 MB (28.21%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cpp
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -1,45 +1,42 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        
-        // Process the most frequent task first
-        unordered_map<char, int> m;
-        queue<pair<int, int>> q;
-        priority_queue<int> pq;
         int time = 0;
+        priority_queue<int> pq;
+        queue<pair<int, int>> q;
+        unordered_map<char, int> cnt;
         
-        for(auto t : tasks){
-            if(m.find(t) == m.end())
-                m[t] = 1;
-            else 
-                m[t]++;
+        // Counting tasks
+        for(auto c : tasks){
+            if(cnt.find(c) == cnt.end())cnt[c] = 1;
+            else cnt[c]++;
+        }
+        for(auto item : cnt){
+            pq.push(item.second);
         }
         
-        for(auto p : m){
-            pq.push(p.second);
-        }
-        cout << pq.size() << endl;
         while(!pq.empty() || !q.empty()){
             
-            // pair[0] = remaining tasks
-            // pair[1] = when available?
+            // Increase the time
             time++;
             
+            // Process it and push it to queue
             if(!pq.empty()){
-                int count = pq.top() - 1; // Because we are using neg values to implement maxHeap
+                int freq = pq.top()-1;
                 pq.pop();
-                if(count){ // If count is not zero
-                    // [Tasks priority, time the task is available again]
-                    pair<int, int> p(count, time + n);
-                    q.push(p);
-                }
+                if(freq > 0)
+                    q.push({freq, time+n});
             }
             
-            if(!q.empty() && q.front().second == time){
-                pq.push(q.front().first);
-                q.pop();
-            }
+            // Push it back to pq if the number of tasks is not zero & the time aligns
+            if(!q.empty()){
+                if(q.front().second <= time){
+                    pq.push(q.front().first);
+                    q.pop();
+                }
+            }            
         }
+        
         
         return time;
     }
