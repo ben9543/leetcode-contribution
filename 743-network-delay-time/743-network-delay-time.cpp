6fbac1691954bfa1f7a46546581664ebf252d88d
Sunbeom Kweon (Ben) <sunbeom.kweon@gmail.com>
2022-10-05 13:50:41 +0900

    Update 743-network-delay-time.cpp

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,51 +1,44 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pair<int, int>>> adj(n + 1, vector<pair<int,int>>());
+        int res = 0;
+        unordered_set<int> visited;
         
-        // 0: weight
-        // 1: destination
-        vector<vector<pair<int, int>>> adj(n+1);
-        
-        for(auto t : times)
-        {
-            int source = t[0];
-            int dest = t[1];
-            int time = t[2];
+        // Creating adjacency list
+        for (int i = 0; i < times.size(); i++) {
+            int source = times[i][0];
+            int dest = times[i][1];
+            int time = times[i][2];
             adj[source].push_back({time, dest});
         }
         
-        // Keep track of minimum time of receiving signal from node k
-        vector<int> recvTimes(n + 1, INT_MAX);
-        recvTimes[k] = 0;
+        // Want to visit smaller weighted edges first
+        // Need to use minHeap
+        priority_queue<pair<int, int>, vector<pair<int,int>>, greater<pair<int,int>>>minHeap;
+        minHeap.push({0,k});
         
-        // Minheap
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({0,k});
-        
-        while(!pq.empty())
-        {
-            int currNode = pq.top().second;
-            int currTime = pq.top().first;
-            pq.pop();
-            if (currTime > recvTimes[currNode]) 
-                continue;
+        while(!minHeap.empty()){
+            int elapsedTime = minHeap.top().first;
+            int currNode = minHeap.top().second;
+            minHeap.pop();
             
-            for(auto item : adj[currNode]){
-                if(recvTimes[item.second] > currTime+item.first){
-                    recvTimes[item.second] = currTime+item.first;
-                    pq.push({currTime+item.first, item.second});
-                }
+            // If the node is already visited, ignore
+            if(visited.find(currNode) != visited.end())continue;
+            
+            // Mark visited nodes
+            visited.insert(currNode);
+            
+            // Upate the max
+            res = max(res, elapsedTime);
+            
+            // Push all attached nodes the minheap with updated time 
+            for(auto p : adj[currNode]){
+                minHeap.push({p.first+elapsedTime, p.second});
             }
-            
         }
         
-        int result = INT_MIN;
-        
-        for (int i = 1; i <= n; i++){
-            result = max(recvTimes[i], result);
-        }
-        
-        return (result==INT_MAX) ? -1 : result;
+        // Return res if we visited all nodes. Else return -1.
+        return (visited.size() == n) ? res : -1;
     }
 };
-
