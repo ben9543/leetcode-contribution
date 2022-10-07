1478f3dd2b225457e845f4976e5833cb33bcfa72
Sunbeom Kweon (Ben) <sunbeom.kweon@gmail.com>
2022-10-07 08:55:41 -0700

    Time: 320 ms (6.48%), Space: 38 MB (28.37%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cpp
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -1,42 +1,42 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        int time = 0;
-        priority_queue<int> pq;
-        queue<pair<int, int>> q;
+        
+        // 1. Count number of tasks
         unordered_map<char, int> cnt;
-        
-        // Counting tasks
         for(auto c : tasks){
-            if(cnt.find(c) == cnt.end())cnt[c] = 1;
-            else cnt[c]++;
-        }
-        for(auto item : cnt){
-            pq.push(item.second);
+            if(cnt.find(c) == cnt.end())
+                cnt[c] = 1;
+            else
+                cnt[c]++;
         }
         
+        // 2. Push the tasks to the priority queue
+        // pair structure: <num of left tasks, available time>
+        priority_queue<int> pq;
+        queue<pair<int,int>> q;
+        for(auto t : cnt){
+            pq.push(t.second);
+        }
+        
+        // 3. Process the priroity queue
+        // We will use a queue to hold processing tasks until the first element of the queue will be available
+        int time = 0;
         while(!pq.empty() || !q.empty()){
-            
-            // Increase the time
             time++;
-            
-            // Process it and push it to queue
             if(!pq.empty()){
-                int freq = pq.top()-1;
+                int task = pq.top();
                 pq.pop();
-                if(freq > 0)
-                    q.push({freq, time+n});
+                if(task-1 > 0)q.push({task-1, time+n});
             }
-            
-            // Push it back to pq if the number of tasks is not zero & the time aligns
             if(!q.empty()){
-                if(q.front().second <= time){
-                    pq.push(q.front().first);
+                auto task = q.front();
+                if(task.second <= time){
                     q.pop();
+                    pq.push(task.first);
                 }
-            }            
+            }
         }
-        
         
         return time;
     }
