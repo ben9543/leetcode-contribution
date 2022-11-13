3bb7cca6ce207a3e54282ce07b33bad84ad30894
Sunbeom Kweon (Ben) <sunbeom.kweon@gmail.com>
2022-11-13 09:04:14 -0800

    Time: 931 ms (37.83%), Space: 8 MB (47.10%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
+        
         for(int i = 0; i < board.size(); i++){
-            for (int j = 0; j < board[0].size(); j++){
-                if(DFS(board, word, 0, i, j))return true;
+            for(int j = 0; j < board[0].size(); j++){
+                if(DFS(board, word, i, j, 0))return true;
             }
         }
+        
         return false;
+        
     }
     
-    bool DFS(vector<vector<char>>& board, string word, int curr, int i, int j){
-        if(curr >= word.length())
-            return true;
-        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || word[curr] != board[i][j])
-            return false;
+    bool DFS(vector<vector<char>>& board, string word, int row, int col, int curr){
         
-        char tmp = board[i][j];
-        board[i][j] = '#';
-
-        if(DFS(board, word, curr+1, i+1, j) || DFS(board, word, curr+1, i-1, j) || DFS(board, word, curr+1, i, j+1) || DFS(board, word, curr+1, i, j-1))
-           return true;
+        // Found
+        if(curr >= word.length())return true;
         
-        board[i][j] = tmp;
+        // Out of bound
+        if(row >= board.size() || col >= board[0].size() || row < 0 || col < 0)return false;
+        
+        // Wrong character
+        if(board[row][col] != word[curr])return false;
+        
+        // Do not visit again
+        char c = board[row][col];
+        board[row][col] = '.';
+        
+        // Search
+        if ( 
+            DFS(board, word, row-1, col, curr+1) ||
+            DFS(board, word, row+1, col, curr+1) ||
+            DFS(board, word, row, col+1, curr+1) ||
+            DFS(board, word, row, col-1, curr+1)
+        ) return true;
+        
+        // Backtrack
+        board[row][col] = c;
         
         return false;
     }
